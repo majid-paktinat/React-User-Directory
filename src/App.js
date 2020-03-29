a37a57be62b20f0f4fc84fa3fe4c54690cf06228
majid <majid.paktinat@gmail.com>
2020-03-29 15:10:15 -0400

    autofucs applied

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
               <div class="row">
                 <div class="col-12">
                   <label for="usernameF">Filter by UserName : </label>
-                  <input type="text" id="usernameF" value={filterValue} onChange={changefilter} autofocus></input>
+                  <input type="text" id="usernameF" value={filterValue} onChange={changefilter} autofocus="true"></input>
                 </div>
               </div>
             </div>
